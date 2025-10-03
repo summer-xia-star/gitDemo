e1ba310230a350a8346fc13d6680952a3c2c5fcb
summmerrrxia <>
2025-10-04 02:43:51 +0800

    hello world

---
+++ helloWorld.m
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // 输出 Hello World
+        NSLog(@"Hello World");
+    }
+    return 0;
+}
